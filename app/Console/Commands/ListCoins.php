824486a89df04c590966e97060abbd1aefe55990
Ivan Morales <ivann_mg@hotmail.com>
2021-03-05 19:33:32 -0600

    Sample command for listing every coin

---
+++ app/Console/Commands/ListCoins.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Coin;
+
+class ListCoins extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'firstapp:coins:list {--name=all}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'List coins on system';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $name = $this->option('name');
+        $coins = null;
+        if ($name !== 'all'){
+            $coins = Coin::where('name', $name)->get();
+        } else {
+            $coins = Coin::all();
+        }
+        $this->info($coins);
+        return 0;
+    }
+}
